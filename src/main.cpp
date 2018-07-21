4341325b03f99c24f33d621e6da79d4ee1998600
JD Reese <jreese@alarm.com>
2018-07-21 01:58:37 -0400

    Rework web server to be async and not block LEDs; generally make it less terrible

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,18 @@
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+// #include <ESP8266WebServer.h>
+#include <ESPAsyncWebServer.h>
 
 #define BREATHE_TIMER 85
 #define GLYPH_STARTUP_TIME 40
 
-#define GAME_TICK_TIME 70
+#define GAME_TICK_TIME 5
 
 const char* wifi_ssid = "DexHunt2018";
 const char* wifi_pw = "dexhunt2018prop";
-ESP8266WebServer server(80);
+//ESP8266WebServer server(80);
+AsyncWebServer server(80);
 
 CRGB leds[NUM_LEDS];
 CRGB currBreatheColorDisabled = CRGB::Black;
@@ -35,7 +36,8 @@ bool gameStatus[NUM_LEDS] = {false};
 int breatheTimer = 0;
 int breatheDelta = 1;
 
-unsigned int gameTimerMs = 50000000;
+unsigned int gameTimerMs = 50000000; //13hrs
+// unsigned int gameTimerMs = 90000;//1.5min
 
 void setup_wifi();
 void manage_wifi();
@@ -60,82 +62,93 @@ void setup() {
 void loop() {
     manage_wifi();
 
-    if (gameStillRunning()) {
-        if (breatheDelta == 1 && breatheTimer < BREATHE_TIMER) {
-            currBreatheColorDisabled = CRGB(0, 20, 255);
-            currBreatheColorEnabled = CRGB(0, 255, 0);
-        }
-        if (breatheDelta == -1 && breatheTimer > 0) {
-            currBreatheColorDisabled = CRGB(0, 50, 50);
-            currBreatheColorEnabled = CRGB(50, 50, 0);
-        }
-
-        //Switch the timer direction if needed then increment
-        if (breatheTimer >= BREATHE_TIMER) breatheDelta = -1;
-        if (breatheTimer <= 0) breatheDelta = 1;
-        breatheTimer += breatheDelta;
-
-        for (int led = 0; led < NUM_LEDS; led++) {
-            if (ledState[led] == LEDState::led_state_breatheoff) {
-                leds[led] = nblend(leds[led], currBreatheColorDisabled, 8);
+    EVERY_N_MILLISECONDS(70) {
+        if (gameStillRunning()) {
+            if (breatheDelta == 1 && breatheTimer < BREATHE_TIMER) {
+                currBreatheColorDisabled = CRGB(0, 20, 255);
+                currBreatheColorEnabled = CRGB(0, 255, 0);
             }
-            else if (ledState[led] == LEDState::led_state_breatheon) {
-                leds[led] = nblend(leds[led], currBreatheColorEnabled, 8);
+            if (breatheDelta == -1 && breatheTimer > 0) {
+                currBreatheColorDisabled = CRGB(0, 50, 50);
+                currBreatheColorEnabled = CRGB(50, 50, 0);
             }
-            else {
-                int whiteoutAmount = map(led_startup_timers[led], GLYPH_STARTUP_TIME, 0, 0, 255);
-                leds[led] = nblend(leds[led], CRGB::White, whiteoutAmount);
     
-                led_startup_timers[led] -= 1;
-                if (led_startup_timers[led] <= 0) {
-                    ledState[led] = LEDState::led_state_breatheon;
+            //Switch the timer direction if needed then increment
+            if (breatheTimer >= BREATHE_TIMER) breatheDelta = -1;
+            if (breatheTimer <= 0) breatheDelta = 1;
+            breatheTimer += breatheDelta;
+    
+            for (int led = 0; led < NUM_LEDS; led++) {
+                if (ledState[led] == LEDState::led_state_breatheoff) {
+                    leds[led] = nblend(leds[led], currBreatheColorDisabled, 8);
+                }
+                else if (ledState[led] == LEDState::led_state_breatheon) {
+                    leds[led] = nblend(leds[led], currBreatheColorEnabled, 8);
+                }
+                else {
+                    int whiteoutAmount = map(led_startup_timers[led], GLYPH_STARTUP_TIME, 0, 0, 255);
+                    leds[led] = nblend(leds[led], CRGB::White, whiteoutAmount);
+        
+                    led_startup_timers[led] -= 1;
+                    if (led_startup_timers[led] <= 0) {
+                        ledState[led] = LEDState::led_state_breatheon;
+                    }
                 }
             }
+        } else {
+            //Game Over
+            if (breatheDelta == 1 && breatheTimer < BREATHE_TIMER) {
+                currBreatheColorEnabled = CRGB(255, 0, 50);
+            }
+            if (breatheDelta == -1 && breatheTimer > 0) {
+                currBreatheColorEnabled = CRGB(100, 0, 20);
+            }
+    
+            //Switch the timer direction if needed then increment
+            if (breatheTimer >= BREATHE_TIMER) breatheDelta = -1;
+            if (breatheTimer <= 0) breatheDelta = 1;
+            breatheTimer += breatheDelta;
+    
+            for (int led = 0; led < NUM_LEDS; led++) {
+                leds[led] = nblend(leds[led], currBreatheColorEnabled, 8);
+            }
         }
-    } else {
-        //Game Over
-        if (breatheDelta == 1 && breatheTimer < BREATHE_TIMER) {
-            currBreatheColorEnabled = CRGB(255, 0, 50);
-        }
-        if (breatheDelta == -1 && breatheTimer > 0) {
-            currBreatheColorEnabled = CRGB(100, 0, 20);
-        }
-
-        //Switch the timer direction if needed then increment
-        if (breatheTimer >= BREATHE_TIMER) breatheDelta = -1;
-        if (breatheTimer <= 0) breatheDelta = 1;
-        breatheTimer += breatheDelta;
-
-        for (int led = 0; led < NUM_LEDS; led++) {
-            leds[led] = nblend(leds[led], currBreatheColorEnabled, 8);
-        }
+       
+        FastLED.show();
     }
-   
-    FastLED.show();
-    delay(GAME_TICK_TIME);
+    
+    // delay(GAME_TICK_TIME);
  
 }
 
-void sendResponse(int code, const char* str) {
-    server.sendHeader("Connection", "close");
-    server.sendHeader("Access-Control-Allow-Origin", "*");
-    server.send(code, "text/plain", str);
-}
-void handleRoot() {
-    sendResponse(200, "DEX Hunt 2018");
+void handleRoot(AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "oh hello there");
+    Serial.write("Dex Hunt 2018 Root");
 }
 
 void handleNotFound(){
-    sendResponse(404, "Not Found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+    // sendResponse(404, "Not Found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
   }
 
-void endpoint_enableLed() {
-    if (!server.hasArg("ledNum") || !server.hasArg("enabled")){ 
-        sendResponse(400, "Missing Parameters");
+void endpoint_enableLed(AsyncWebServerRequest *request) {
+    int idx = -1;
+    int enabled = -1;
+    int paramsCount = request->params();
+    for (int i = 0; i < paramsCount; i++) {
+        AsyncWebParameter* p = request->getParam(i);
+        if (p->name() == "ledNum") {
+            idx = p->value().toInt();
+        }
+        if (p->name() == "enabled") {
+            enabled = p->value().toInt();
+        }
+    }
+
+    if (idx == -1 || enabled == -1) {
+        request->send(400, "text/plain", "Missing Params");
         return;
     }
-    int idx = server.arg("ledNum").toInt();
-    int enabled = server.arg("enabled").toInt();
+
     if (idx >= 0 && idx < NUM_LEDS) {
         if (enabled == 0) {
             led_startup_timers[idx] = 0;
@@ -153,27 +166,34 @@ void endpoint_enableLed() {
             serialWriteValue(idx);
             Serial.write("\n");
         }
-        sendResponse(200, "OK");
+        request->send(200, "text/plain", "OK");
     } else {
-        sendResponse(400, "Invalid Parameters");
+        request->send(400, "text/plain", "Invalid Params");
         return;
     }
 }
 
-void endpoint_setTimeRemaining() {
-    if (!server.hasArg("seconds")){ 
-        sendResponse(400, "Missing Parameters");
-        return;
+void endpoint_setTimeRemaining(AsyncWebServerRequest *request) {
+    int seconds = 0;
+    int paramsCount = request->params();
+    for (int i = 0; i < paramsCount; i++) {
+        AsyncWebParameter* p = request->getParam(i);
+        if (p->name() == "seconds") {
+            seconds = p->value().toInt();
+            Serial.println("Param Name");
+            Serial.println(p->name());
+            Serial.println("Param Value");
+            Serial.println(p->value());
+        }
     }
-    int seconds = server.arg("seconds").toInt();
     gameTimerMs = seconds * 1000;
     Serial.write("Setting Time Remaining: ");
     serialWriteValue(gameTimerMs);
     Serial.write("\n");
-    sendResponse(200, "OK");
+    request->send(200, "text/plain", "OK");
 }
 
-void endpoint_getGameStatus() {
+void endpoint_getGameStatus(AsyncWebServerRequest *request) {
     int gameStatusCount = 0;
     int i;
     for (i = 0; i < NUM_LEDS; i++) {
@@ -181,10 +201,10 @@ void endpoint_getGameStatus() {
     }
     char timestr[16];
     sprintf(timestr, "%d", gameStatusCount);
+    request->send(200, "text/plain", timestr);
     Serial.write("Game Status: ");
     serialWriteValue(gameStatusCount);
     Serial.write("\n");
-    sendResponse(200, timestr);
 }
 
 bool gameStillRunning() {
@@ -200,10 +220,10 @@ void setup_wifi() {
 //   WiFi.mode(WIFI_STA);
   WiFi.begin(wifi_ssid, wifi_pw);
 
-  server.client().setNoDelay(true);
+//   server.client().setNoDelay(true);
 
   server.on("/", handleRoot);
-  server.onNotFound(handleNotFound);
+//   server.onNotFound(handleNotFound);
   server.on("/setEnabled", endpoint_enableLed);
   server.on("/setTimeRemaining", endpoint_setTimeRemaining);
   server.on("/getGameStatus", endpoint_getGameStatus);
@@ -225,8 +245,8 @@ void manage_wifi() {
     } else if (wifiStatus == WL_DISCONNECTED) {
         if (wifiIsConnected) {
             wifiIsConnected = false;
-            server.stop();
+            //server.stop();
         }
     }
-    server.handleClient();
+    // server.handleClient();
 }
